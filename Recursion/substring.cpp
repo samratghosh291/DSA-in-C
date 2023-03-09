a48e1bcadcd17fcbe25b8c52392eb9bc7d36a1e0
Samrat Ghosh <samratghosh490@gmail.com>
2023-03-09 21:37:49 +0530

    recursion

---
+++ Recursion/substring.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void subseq(string s, string ans)
+{
+
+    if (s.length() == 0)
+    {
+        cout << ans << endl;
+        return;
+    }
+
+    char ch = s[0];
+    string ros = s.substr(1);
+
+    subseq(ros, ans);
+    subseq(ros, ans + ch);
+
+    return;
+}
+
+int main()
+{
+
+    string s;
+    cout << "Enter the string: ";
+    cin >> s;
+    subseq(s, "");
+
+    return 0;
+}
