5f9667e907262d57bdc941430bc7cb5c5f60fa74
Samrat Ghosh <samratghosh490@gmail.com>
2023-03-09 15:31:44 +0530

    update ToH

--- Recursion/tower_Of_Honoi.cpp
+++ Recursion/tower_Of_Honoi.cpp
@@ -9,7 +9,7 @@ void towerOfHonoi(int n,char src, char dest, char helper){
     }
 
     towerOfHonoi(n-1,src,helper,dest);
-    cout<<"Move "<<src<<" to "<<dest<<endl;
+    cout<<"Move "<<src<<" "<<"block"<<" to "<<dest<<endl;
     towerOfHonoi(n-1,helper,dest,src);
  return;
 
@@ -17,8 +17,10 @@ void towerOfHonoi(int n,char src, char dest, char helper){
 
 int main()
 {
-
-    towerOfHonoi(3,'A','C','B');
+    int n;
+    cout<<"Enter the no. of block: ";
+    cin>>n;
+    towerOfHonoi(n,'A','C','B');
     
     return 0;
 }
