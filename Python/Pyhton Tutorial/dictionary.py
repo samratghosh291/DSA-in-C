09864782399307d1d838cb33da855b4d9eb696d1
Samrat Ghosh <samratghosh490@gmail.com>
2023-03-09 17:58:42 +0530

    update

---
+++ Python/Pyhton Tutorial/dictionary.py
@@ -0,0 +1,20 @@
+
+
+# Dictionary holds key:value pair. Key-Value is provided in the dictionary to make it more optimized.
+
+dict={1: "piku",2: "pinky",3: "papa"}
+print(dict)
+
+print("\n")
+
+# Creating a Dictionary
+# with Integer Keys
+Dict = {1: 'Geeks', 2: 'For', 3: 'Geeks'}
+print("\nDictionary with the use of Integer Keys: ")
+print(Dict)
+
+# Creating a Dictionary
+# with Mixed keys
+Dict = {'Name': 'Geeks', 1: [1, 2, 3, 4]}
+print("\nDictionary with the use of Mixed Keys: ")
+print(Dict)
